cc940b91028dbb6af68ee4e5e8f795c2c1a9e88a
Dom Hallan <dom@domhallan.com>
2025-12-29 03:07:41 -0500

    Refactor print statements and main function syntax in hello.py for consistency
    - Updated print statement to use single quotes for uniformity.
    - Adjusted indentation of the main function call for improved readability.

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def main():
-    print("Hello from from-devops-to-platform-engineering-master-backstage-and-idps!")
+  print('Hello from from-devops-to-platform-engineering-master-backstage-and-idps!')
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+  main()
